29dee62a14652bdb1ceda00b069a3d5dec2b299c
Николай Синьков <nikolay.sinkov97@gmail.com>
2023-02-04 13:39:52 +0300

    Project 2. Community. Review 1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,20 +4,24 @@ from .models import Post, Group
 
 
 def index(request):
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
     posts = Post.objects.order_by('-pub_date')[:10]
-    title = 'Это гдавная страница проекта Yatube'
     context = {
         'title': title,
-        'posts': posts
+        'posts': posts,
     }
-    return render(request, 'posts/index.html', context, title)
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
+    template = 'posts/group_list.html'
+    title = 'Группы постов'
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
+        'title': title,
         'group': group,
         'posts': posts,
     }
-    return render(request, 'posts/group_list.html', context)
+    return render(request, template, context)
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
+    path('group/<slug:slug>/', include('posts.urls', namespace='group')),
+    path('admin/', admin.site.urls),
 ]
